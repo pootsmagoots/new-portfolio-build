eefd8c4e74049736e948c86330b22aa34e115c66
Travis <Travisbatts6@gmail.com>
2017-11-07 13:49:45 -0500

    major GULP changes and auto save functionality added

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,13 @@
-var gulp = require('gulp');
-watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nestedcss = require('postcss-nested'),
-importcss = require('postcss-import');
-
-gulp.task('default', function(){
-  console.log("hurray you created a gulp task");
-});
+require('./gulp/tasks/styles')
+require('./gulp/tasks/watch')
 
 
-gulp.task('html', function(){
-  console.log("imagine something useful being done to your html");
-});
 
-gulp.task('styles', function(){
-  return gulp.src('./app/assets/styles/styles.css')
-  .pipe(postcss([importcss, cssvars, nestedcss, autoprefixer]))
-  .pipe(gulp.dest('./app/temp/styles'));
-});
-
-gulp.task('watch', function(){
-
-  watch ('./app/index.html', function(){
-    gulp.start('html');
-  });
-
-  watch('./app/assets/styles/**/*.css', function(){
-    gulp.start('styles');
-  })
-
-})
+// gulp.task('default', function(){
+//   console.log("hurray you created a gulp task");
+// });
+//
+//
+// gulp.task('html', function(){
+//   console.log("imagine something useful being done to your html");
+// });
