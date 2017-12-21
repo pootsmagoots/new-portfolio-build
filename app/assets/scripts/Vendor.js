dc4ba64eabf7a6d45f884dc5177d136173918f0a
Travis <Travisbatts6@gmail.com>
2017-12-21 15:15:35 -0500

    about to launch online

--- app/assets/scripts/Vendor.js
+++ app/assets/scripts/Vendor.js
@@ -1,1 +1,4 @@
-import 'lazysizes';
+//Browser support//
+import '../../temp/scripts/modernizr'; //modernizr file //
+import 'lazysizes';  //allows images to be loaded on scroll  data-srcset on images //
+import 'picturefill'; //allows browsers to accepts srcset and picture //
