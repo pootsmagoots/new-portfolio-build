391f80c84c98553f60abd0a375a49c7d7903f313
Travis <Travisbatts6@gmail.com>
2017-12-28 22:28:02 -0500

    added some font awesome icons

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -7,6 +7,7 @@ importcss = require('postcss-import'),
 mixins = require('postcss-mixins'),
 hexRGBA = require('postcss-hexrgba');
 
+
 gulp.task('styles', function(){
   return gulp.src('./app/assets/styles/styles.css')
   .pipe(postcss([importcss, mixins, cssvars, nestedcss, hexRGBA, autoprefixer]))
